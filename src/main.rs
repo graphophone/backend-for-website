f4c3146c0c20a0c8a3c0cc6fadd9b49444c6d589
justkinou <justkinou@proton.me>
2026-09-03 23:48:29 +0200

    setup basic artix http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
