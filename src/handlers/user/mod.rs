23bb86837d884a5d7ca2126b7dd705333ca6aa57
justkinou <justkinou@proton.me>
2026-09-25 23:20:52 +0200

    feat: add avatar change handling

--- src/handlers/user/mod.rs
+++ src/handlers/user/mod.rs
@@ -1,4 +1,4 @@
-use axum::{Extension, Json, Router, extract::{Path, State}, http::StatusCode, middleware::from_fn_with_state, response::{IntoResponse, Response}, routing::{get, put}};
+use axum::{Extension, Json, Router, extract::{Multipart, Path, State}, http::StatusCode, middleware::from_fn_with_state, response::{IntoResponse, Response}, routing::{get, put}};
 use axum_cookie::CookieLayer;
 use tonic::Code;
 use crate::{clients::{auth::AuthClient, identity::{IdentityClient, identity_grpc}}, handlers::{error::HandlerError, user::dto::{BasicProfileRes, EditProfileReq, ProfileRes}}, middleware::auth::auth_middleware, util::assets::asset_key_to_url};
@@ -22,8 +22,8 @@ async fn get_user_profile(
         Ok(res) => {
             let profile = res.into_inner();
             
-            let avatar_url = asset_key_to_url("avatar", profile.avatar_key);
-            let banner_url = asset_key_to_url("banner", profile.banner_key);
+            let avatar_url = asset_key_to_url(profile.avatar_id);
+            let banner_url = asset_key_to_url(profile.banner_id);
 
             let res = ProfileRes {
                 user_id: profile.user_id,
@@ -55,7 +55,7 @@ async fn get_my_basic_profile(
         Ok(res) => {
             let res = res.into_inner();
 
-            let avatar_url = asset_key_to_url("avatar", res.avatar_key);
+            let avatar_url = asset_key_to_url(res.avatar_id);
             Ok(Json(BasicProfileRes {
                 user_id: res.user_id,
                 username: res.username,
@@ -78,8 +78,8 @@ async fn get_my_full_profile(
         Ok(res) => {
             let res = res.into_inner();
 
-            let avatar_url = asset_key_to_url("avatar", res.avatar_key);
-            let banner_url = asset_key_to_url("banner", res.banner_key);
+            let avatar_url = asset_key_to_url(res.avatar_id);
+            let banner_url = asset_key_to_url(res.banner_id);
 
             Ok(Json(ProfileRes {
                 user_id: res.user_id,
@@ -128,6 +128,76 @@ async fn edit_my_profile(
     }
 }
 
+#[axum::debug_handler]
+async fn edit_my_avatar(
+    State(state): State<UserState>,
+    Extension(user_id): Extension<i64>,
+    image_multipart: Option<Multipart>,
+) -> Result<Response, HandlerError> {
+    let mut identity_client = state.identity_client;
+
+    let image_data = match image_multipart {
+        Some(mut image_multipart) => {
+            let field = image_multipart.next_field()
+                .await
+                .map_err(|_| HandlerError::BadRequest("Image has to be sent".to_string()))?;            
+            let bytes = match field {
+                Some(field) => {
+                    let mime_type = match field.content_type() {
+                        Some(v) => {
+                            let split = v.split("/");
+                            let mime_type = split.skip(1).next();
+                            match mime_type {
+                                Some(v) => v.to_string(),
+                                None => return Err(HandlerError::BadRequest("Couldn't get mime type".to_string())),
+                            }
+                        },
+                        None => return Err(HandlerError::BadRequest("Couldn't get mime type".to_string())),
+                    };
+
+                    let bytes = field.bytes()
+                        .await
+                        .map_err(|_| {
+                            HandlerError::BadRequest("Image has to be sent".to_string())
+                        })?
+                        .to_vec();
+                    
+                    (bytes, mime_type)
+                },
+                None => return Err(HandlerError::BadRequest("Image has to be sent".to_string())),
+            };
+            Some(bytes)
+        },
+        None => None,
+    };
+
+    let image_payload = match image_data {
+        Some((image_bytes, mime_type)) => {
+            let image = identity_grpc::Image {
+                image: image_bytes,
+                mime_type,
+            };
+            Some(image)
+        },
+        None => None,
+    };
+
+    let req = identity_grpc::UpdateAvatarReq {
+        user_id,
+        avatar: image_payload,
+    };
+    match identity_client.update_avatar(req).await {
+        Ok(_) => Ok(StatusCode::OK.into_response()),
+        Err(e) => {
+            if e.code().eq(&Code::AlreadyExists) {
+                Err(HandlerError::Conflict)
+            } else {
+                Err(HandlerError::InternalError)
+            }
+        },
+    }
+}
+
 pub fn create_user_router(
     auth_client: AuthClient,
     identity_client: IdentityClient,
@@ -138,6 +208,7 @@ pub fn create_user_router(
             .route("/me", get(get_my_basic_profile))
             .route("/full-profile", get(get_my_full_profile))
             .route("/edit-profile", put(edit_my_profile))
+            .route("/edit-avatar", put(edit_my_avatar))
             .layer(from_fn_with_state(auth_client, auth_middleware))
             .layer(CookieLayer::strict())
         )
--- src/util/assets.rs
+++ src/util/assets.rs
@@ -1,3 +1,3 @@
-pub fn asset_key_to_url(prefix: &str, key: Option<String>) -> Option<String> {
-    key.map(|v| format!("/{prefix}/{v}"))
+pub fn asset_key_to_url(key: Option<String>) -> Option<String> {
+    key.map(|v| format!("/assets/{v}"))
 }
